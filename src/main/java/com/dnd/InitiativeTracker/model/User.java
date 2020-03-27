292d7a29ceb923f9229673ac21de139dca67ee74
Annelies van Hoesel <avanhoesel@gmail.com>
2020-03-27 15:30:19 +0100

    findByUsername test

--- src/main/java/com/dnd/InitiativeTracker/model/User.java
+++ src/main/java/com/dnd/InitiativeTracker/model/User.java
@@ -47,6 +47,47 @@ public class User {
         encounters.remove(encounter);
     }
 
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Encounter> getEncounters() {
+        return encounters;
+    }
+
+    public void setEncounters(List<Encounter> encounters) {
+        this.encounters = encounters;
+    }
+
     @Override
     public String toString() {
         return username;
---
+++ src/test/java/com/dnd/InitiativeTracker/model/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.dnd.InitiativeTracker.model;
+
+import com.dnd.InitiativeTracker.model.repository.UserRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.Optional;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRep;
+
+    /*
+    Twee manieren om findByUsername te testen, niet vergeten properties goed te zetten voor virtuele db
+    #spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect
+    spring.jpa.database-platform=org.hibernate.dialect.H2Dialect
+    */
+
+    @Test
+    public void whenFindByName_thenReturnUser() {
+        User alex = new User("alex");
+        alex.setEmail("ales@hotmail.com");
+        alex.setPassword("ingewikkeldwachtwoord");
+        entityManager.persist(alex);
+        entityManager.flush();
+        Optional<User> fromDB = userRep.findByUsername(alex.getUsername());
+        assertThat(fromDB.get().getUsername()).isEqualTo(alex.getUsername());
+    }
+
+    @Test
+    void findByUsername() {
+        User testUser = new User("Piet");
+        testUser.setEmail("Piet@hotmail.com");
+        testUser.setPassword("wachtwoord");
+        userRep.save(testUser);
+        Optional<User> gevonden = userRep.findByUsername("Piet");
+        if (gevonden.isPresent()) {
+            User test = gevonden.get();
+            assertThat(test.getUsername()).isEqualTo(testUser.getUsername());
+        }
+    }
+}
